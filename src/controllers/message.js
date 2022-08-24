508518de0425f68166aaa9f01f0df7cd43c940d1
Ayuri Bessa <ayuribessa@gmail.com>
2022-08-24 10:50:17 -0300

    adiciona twilio cliente e refatora controller

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -1,12 +1,17 @@
 import * as MessageService from "../services/message.js";
 
-function message(request, response) {
+async function message(request, response) {
     // const message = request.body.message;
     // const phoneNumber = request.body.phoneNumber;
     //refatorando codigo acima com desestruturação
     const { message, phoneNumber } = request.body;
-    const responseMessage = MessageService.sendMessage(message, phoneNumber);
-    response.send(responseMessage);
+    try {
+        const responseMessage = await MessageService.sendMessage(message, phoneNumber);
+        response.send(responseMessage);
+    }
+    catch (error) {
+        response.status(error.status).send(error);
+    }
 }
 
 export { message }
